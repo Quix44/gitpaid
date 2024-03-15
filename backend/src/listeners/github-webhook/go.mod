9a07f665895cbd3b3216830c7bb689519b04df43
Daniel Huffer <danielhuffer@live.com>
2024-03-15 23:17:37 +0100

    Start testing issues and labels via git hooks

--- backend/src/listeners/github-webhook/go.mod
+++ backend/src/listeners/github-webhook/go.mod
@@ -7,4 +7,19 @@ require (
 	github.com/google/go-github v17.0.0+incompatible
 )
 
-require github.com/google/go-querystring v1.1.0 // indirect
+require (
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.30.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.20.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.9.4 // indirect
+	github.com/aws/smithy-go v1.20.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.25.3
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.13.9
+	github.com/google/go-querystring v1.1.0 // indirect
+)
--- backend/src/listeners/github-webhook/src/main.go
+++ backend/src/listeners/github-webhook/src/main.go
@@ -5,15 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/google/go-github/github"
 )
+
+var svc *dynamodb.Client
+
+const tableName = "gitpaid"
 
 type LambdaEvent struct {
 	Headers map[string]string
 	Event   string
+}
+
+type DynamoIssuesEventItem struct {
+	ID        string              `json:"id" dynamodbav:"id"`
+	Typename  string              `json:"typename" dynamodbav:"typename"`
+	CreatedAt time.Time           `json:"createdAt" dynamodbav:"createdAt"`
+	UpdatedAt time.Time           `json:"updatedAt" dynamodbav:"updatedAt"`
+	Data      *github.IssuesEvent `json:"data" dynamodbav:"data"`
+	Metadata  map[string]string   `json:"metadata" dynamodbav:"metadata"`
+}
+
+type DynamoLabelEventItem struct {
+	ID        string             `json:"id" dynamodbav:"id"`
+	Typename  string             `json:"typename" dynamodbav:"typename"`
+	CreatedAt time.Time          `json:"createdAt" dynamodbav:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt" dynamodbav:"updatedAt"`
+	Data      *github.LabelEvent `json:"data" dynamodbav:"data"`
+	Metadata  map[string]string  `json:"metadata" dynamodbav:"metadata"`
 }
 
 func init() {
@@ -50,8 +77,11 @@ func HandleRequest(ctx context.Context, r LambdaEvent) (events.APIGatewayProxyRe
 	switch event := eventObject.(type) {
 	case *github.IssuesEvent:
 		log.Printf("Received issue event: %+v\n", event)
+		putIssuesEvent(ctx, event, r.Headers)
+
 	case *github.LabelEvent:
 		log.Printf("Label Event: %+v\n", event)
+		putLabelEvent(ctx, event)
 	default:
 		log.Printf("Unknown event type: %+v\n", event)
 	}
@@ -64,6 +94,49 @@ func HandleRequest(ctx context.Context, r LambdaEvent) (events.APIGatewayProxyRe
 		Body:            "{\"message\": \"All Good!\"}",
 		IsBase64Encoded: false,
 	}, nil
+}
+
+func putLabelEvent(ctx context.Context, event *github.LabelEvent) {
+	item := DynamoLabelEventItem{
+		ID:        strconv.FormatInt(*event.Label.ID, 10),
+		Typename:  "Label",
+		CreatedAt: time.Now().UTC(),
+		Data:      event,
+	}
+	// check if item exists
+	PutItemInDynamoDB(ctx, item)
+}
+
+func putIssuesEvent(ctx context.Context, event *github.IssuesEvent, metaData map[string]string) {
+	item := DynamoIssuesEventItem{
+		ID:        strconv.FormatInt(*event.Issue.ID, 10),
+		Typename:  "Issue",
+		CreatedAt: time.Unix(0, event.Issue.UpdatedAt.UnixNano()).UTC(),
+		Data:      event,
+		Metadata:  metaData,
+	}
+
+	// check if item exists
+	PutItemInDynamoDB(ctx, item)
+}
+
+func PutItemInDynamoDB[T any](ctx context.Context, item T) error {
+	// Marshal the item into a map of DynamoDB attribute values
+	av, err := attributevalue.MarshalMap(item)
+	if err != nil {
+		return fmt.Errorf("failed to marshal item into DynamoDB attribute values: %w", err)
+	}
+
+	// Put the item into the specified DynamoDB table
+	_, err = svc.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      av,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to put item into DynamoDB table: %w", err)
+	}
+
+	return nil
 }
 
 func main() {
