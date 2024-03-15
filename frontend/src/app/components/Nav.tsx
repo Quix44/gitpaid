2591f927bd2ffadcb11fa576aef95bbc6baa3ad2
Soheima Maradiaga <soheimam@gmail.com>
2024-03-15 23:29:58 +0000

    added items center

--- frontend/src/app/components/Nav.tsx
+++ frontend/src/app/components/Nav.tsx
@@ -5,9 +5,9 @@ import Provider from '../(providers)/Wallet';
 
 function Nav() {
     return (
-        <nav className="flex justify-between py-8 px-24">
+        <nav className="flex justify-between py-8 px-24 items-center">
             <Image src="/logo.svg" alt="logo" width={112} height={40} />
-            <div className="flex space-between">
+            <div className="flex space-between items-center">
                 <Button variant="link">Git Started</Button>
                 <Button variant="link">Git Help</Button>
                 <Provider />
