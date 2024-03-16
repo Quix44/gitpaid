1394ae77f16b6cb722aba8cf99611be9e12af5c4
Soheima Maradiaga <soheimam@gmail.com>
2024-03-16 21:37:15 +0000

    fixed padding

--- frontend/src/app/repositories/page.tsx
+++ frontend/src/app/repositories/page.tsx
@@ -11,7 +11,7 @@ function Page({
 
     return (
         <main className="p-24">
-            <div className="flex items-center justify-between space-y-2">
+            <div className="flex items-center justify-between space-y-2 p-8">
                 <div>
                     <h2 className="text-2xl font-bold tracking-tight title text-primary">Here are your repositories</h2>
                     <p className="text-muted-foreground">
