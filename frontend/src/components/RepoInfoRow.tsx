11422aae896b3708f18ad0df075551110c99cef4
Irine Stephanie <pinxhoes@gmail.com>
2024-03-16 22:56:34 +0000

    changed repo info colors

--- frontend/src/components/RepoInfoRow.tsx
+++ frontend/src/components/RepoInfoRow.tsx
@@ -1,38 +1,46 @@
 
 function RepoInfoRow() {
     return (
-        <section className="grid grid-cols-12 my-3 w-full">
-            <div className="grid col-span-4 bg-red-900 grid-cols-subgrid rounded-md items-center h-[158px]">
+        <section className="grid grid-cols-12 my-3 w-full space-x-5">
+            <div className="grid col-span-4 rounded-lg border backdrop-blur-xl bg-card text-card-foreground shadow-sm grid-cols-subgrid items-center h-[158px] ">
                 <div className="col-span-2">
-                    <div className="grid col-span-2 justify-center items-center rounded-full bg-[#E9265C] w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">1</h1></div>
+                    <div className="grid col-span-2 justify-center items-center rounded-full bg-gradient w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">1</h1></div>
                 </div>
                 <div className="col-span-2 ">
                     <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0">
                         CONNECT
                     </h2>
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Your Repositories</h4>
+                    <p className="leading-7 [&:not(:first-child)]:mt-0">
+                        Your Repositories
+                    </p>
                 </div>
             </div>
-            <div className="grid col-span-4 grid-cols-subgrid items-center bg-orange-400 rounded-md mx-2">
+
+            <div className="grid col-span-4 grid-cols-subgrid items-center rounded-lg border backdrop-blur-xl bg-card text-card-foreground shadow-sm mx-2">
                 <div className="col-span-2">
-                    <div className="grid col-span-2 justify-center items-center rounded-full bg-[#F1D090] w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">2</h1></div>
+                    <div className="grid col-span-2 justify-center items-center rounded-full bg-gradient w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">2</h1></div>
                 </div>
                 <div className="col-span-2 ">
                     <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0">
                         FUND
                     </h2>
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">With Your Tokens</h4>
+                    <p className="leading-7 [&:not(:first-child)]:mt-0">
+                        With Your Tokens
+                    </p>
                 </div>
             </div>
-            <div className="grid col-span-4 grid-cols-subgrid items-center bg-lime-700 rounded-md">
+            <div className="grid col-span-4 grid-cols-subgrid items-center rounded-lg border backdrop-blur-xl bg-card text-card-foreground shadow-sm">
                 <div className="col-span-2">
-                    <div className="grid col-span-2 justify-center items-center rounded-full bg-[#34AC80] w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">3</h1></div>
+                    <div className="grid col-span-2 justify-center items-center rounded-full bg-gradient w-32 h-32 ml-3"><h1 className=" col-span-5 scroll-m-20 font-extrabold tracking-tight text-8xl items-center">3</h1></div>
                 </div>
                 <div className="col-span-2 ">
                     <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0">
                         APPLY
                     </h2>
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Your Webhook URL</h4>
+                    <p className="leading-7 [&:not(:first-child)]:mt-0">
+                        Your Webhook URL
+                    </p>
+
                 </div>
             </div>
 
